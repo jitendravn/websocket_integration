94eb3d773dc1c76363665cd1baa26d9e6bd496d0
JitendraCodoffer <jitendra.codoffer@gmail.com>
2022-06-07 16:23:04 +0530

    Websocket initegration

---
+++ resources/views/chats/chat.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row shadow p-2" style="height:100vh">
+            <div class="col-sm-4">
+                <div class="card shadow">
+                    <div class="card-header text-dark fw-bold bg-light">
+                        Users
+                    </div>
+                    <div class="card-body">
+                        <ul style="list-style: none">
+                            <li>Jitendra</li>
+                            <li>Mohit</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="card shadow">
+                    <div class="card-header text-dark fw-bold bg-light">
+                        Chat
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
